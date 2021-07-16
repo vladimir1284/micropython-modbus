abc31de1c3af9046b85eb60ca67ce75ad72fcfad
Jonas Scharpf <jonas@brainelectronics.de>
2021-07-16 16:40:30 +0200

    split modbus processing functions into sub-functions

--- modbus.py
+++ modbus.py
@@ -9,9 +9,6 @@ from uModbus.serial import Serial
 from uModbus.tcp import TCPServer
 import uModbus.const as ModbusConst
 from machine import UART
-from machine import Pin
-import _thread
-import time
 
 
 class Modbus(object):
@@ -30,147 +27,183 @@ class Modbus(object):
         return True
 
     def _process_req(self, request):
-        if request.function == ModbusConst.READ_DISCRETE_INPUTS:
-            print('READ_DISCRETE_INPUTS on {}'.format(request.register_addr))
-
-            if request.register_addr >= 101 and request.register_addr <= 102:
-                vals = []
-                upper_limit = request.register_addr + request.quantity
-                for i in range(request.register_addr, upper_limit):
-                    if i == 101:
-                        # vals.append(self._exo.DI1())
-                        vals.append(101)
-                    elif i == 102:
-                        # vals.append(self._exo.DI2())
-                        vals.append(102)
-                    else:
-                        request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
-                        break
-                request.send_response(vals)
-            else:
-                request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
-        elif request.function == ModbusConst.READ_COILS:
-            print('READ_COILS on {}'.format(request.register_addr))
-
-            if request.register_addr == 201:
-                # request.send_response([self._exo.DO1()])
-                request.send_response([201])
-            elif request.register_addr == 151:
-                # ttl1 = Pin(self._exo.PIN_TTL1, mode=Pin.IN, pull=None)
-                # request.send_response([ttl1()])
-                request.send_response([151])
-            elif request.register_addr == 152:
-                # ttl2 = Pin(self._exo.PIN_TTL2, mode=Pin.IN, pull=None)
-                # request.send_response([ttl2()])
-                request.send_response([152])
-            else:
-                request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
-        elif request.function == ModbusConst.WRITE_SINGLE_COIL:
-            print('WRITE_SINGLE_COIL on {}'.format(request.register_addr))
-
-            if request.register_addr == 151:
-                # ttl1 = Pin(self._exo.PIN_TTL1, mode=Pin.OUT)
-                if request.data[0] == 0x00:
-                    print('Set coil to 0x0')
-                    # ttl1(0)
-                    request.send_response()
-                elif request.data[0] == 0xFF:
-                    print('Set coil to 0xFF')
-                    # ttl1(1)
-                    request.send_response()
-                else:
-                    request.send_exception(ModbusConst.ILLEGAL_DATA_VALUE)
-            elif request.register_addr == 152:
-                # ttl2 = Pin(self._exo.PIN_TTL2, mode=Pin.OUT)
-                if request.data[0] == 0x00:
-                    print('Set coil to 0x0')
-                    # ttl2(0)
-                    request.send_response()
-                elif request.data[0] == 0xFF:
-                    print('Set coil to 0xFF')
-                    # ttl2(1)
-                    request.send_response()
-                else:
-                    request.send_exception(ModbusConst.ILLEGAL_DATA_VALUE)
-            elif request.register_addr == 201:
-                if request.data[0] == 0x0:
-                    print('Set coil to 0x0')
-                    # self._exo.DO1(0)
-                    request.send_response()
-                elif request.data[0] == 0xFF:
-                    print('Set coil to 0xFF')
-                    # self._exo.DO1(1)
-                    request.send_response()
-                else:
-                    request.send_exception(ModbusConst.ILLEGAL_DATA_VALUE)
-            else:
-                request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+        if request.function == ModbusConst.READ_COILS:
+            self._process_read_coils(request=request)
+        elif request.function == ModbusConst.READ_DISCRETE_INPUTS:
+            self._process_read_ists(request=request)
+        elif request.function == ModbusConst.READ_HOLDING_REGISTERS:
+            self._process_read_hregs(request=request)
         elif request.function == ModbusConst.READ_INPUT_REGISTER:
-            print('READ_INPUT_REGISTER on {}'.format(request.register_addr))
-
-            if request.register_addr >= 301 and request.register_addr <= 307:
-                vals = []
-                signed = []
-                for i in range(request.register_addr, request.register_addr + request.quantity):
-                    if i == 301:
-                        # vals.append(int(self._exo.thpa.temperature() * 10))
-                        vals.append(301)
-                        signed.append(True)
-                    elif i == 302:
-                        # vals.append(int(self._exo.thpa.humidity() * 10))
-                        vals.append(302)
-                        signed.append(False)
-                    elif i == 303:
-                        # vals.append(int(self._exo.thpa.pressure() * 10))
-                        vals.append(303)
-                        signed.append(False)
-                    elif i == 304:
-                        # vals.append(int(self._exo.thpa.gas_resistance() / 1000))
-                        vals.append(304)
-                        signed.append(False)
-                    elif i == 305:
-                        # vals.append(int(self._exo.light.lux() * 10))
-                        vals.append(305)
-                        signed.append(False)
-                    elif i == 306:
-                        # vals.append(self._exo.sound.avg())
-                        vals.append(306)
-                        signed.append(False)
-                    elif i == 307:
-                        vals.append(307)
-                        # vals.append(self._exo.sound.peak())
-                        signed.append(False)
-                    elif i == 308:
-                        vals.append(308)
-                        # vals.append(self._exo.thpa.iaq())
-                        signed.append(False)
-                    elif i == 309:
-                        vals.append(309)
-                        # vals.append(self._exo.thpa.iaq_trend())
-                        signed.append(True)
-                    else:
-                        request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
-                        break
-                request.send_response(vals, signed=signed)
-            else:
-                request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+            self._process_read_iregs(request=request)
+        elif request.function == ModbusConst.WRITE_SINGLE_COIL:
+            self._process_write_coils(request=request)
         elif request.function == ModbusConst.WRITE_SINGLE_REGISTER:
-            print('WRITE_SINGLE_REGISTER on {}'.format(request.register_addr))
-
-            if request.register_addr == 401:
-                val = request.data_as_registers(signed=False)[0]
-                print('Set register to {}'.format(val))
-                # _thread.start_new_thread(self._beep, (val,))
-                request.send_response()
-            elif request.register_addr == 211:
-                val = request.data_as_registers(signed=False)[0]
-                print('Set register to {}'.format(val))
-                # _thread.start_new_thread(self._do1_pulse, (val,))
-                request.send_response()
-            else:
-                request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+            self._process_write_hregs(request=request)
         else:
             request.send_exception(ModbusConst.ILLEGAL_FUNCTION)
+
+    def _process_read_coils(self, request):
+        """
+        Process access to coil register
+
+        Coils (only getter) [0, 1], function 01 - read single register
+
+        :param      request:  The request
+        :type       request:  Request
+        """
+        print('READ_COIL of ID #{}'.format(request.register_addr))
+
+        if request.register_addr == 101:
+            request.send_response([1])
+        elif request.register_addr == 102:
+            request.send_response([1])
+        else:
+            request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+
+    def _process_read_ists(self, request):
+        """
+        Process access to discrete input register
+
+        Ists (only getter) [0, 1], function 02 - read input status (discrete
+        inputs/digital input)
+
+        :param      request:  The request
+        :type       request:  Request
+        """
+        print('READ_DISCRETE_INPUTS of ID #{}'.format(request.register_addr))
+
+        if request.register_addr >= 101 and request.register_addr <= 102:
+            vals = []
+            upper_limit = request.register_addr + request.quantity
+            for i in range(request.register_addr, upper_limit):
+                if i == 101:
+                    vals.append(1)
+                elif i == 102:
+                    vals.append(1)
+                else:
+                    request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+                    break
+            request.send_response(vals)
+        else:
+            request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+
+    def _process_read_hregs(self, request):
+        """
+        Process access to holding register
+
+        Hregs (setter+getter) [0, 65535], function 03 - read holding register
+
+        :param      request:  The request
+        :type       request:  Request
+        """
+        print('READ_HOLDING_REGISTERS of ID #{}'.format(request.register_addr))
+
+        if request.register_addr >= 101 and request.register_addr <= 102:
+            vals = []
+            signed = []
+            for i in range(request.register_addr, request.register_addr + request.quantity):
+                if i == 101:
+                    vals.append(101)
+                    signed.append(True)
+                elif i == 102:
+                    vals.append(102)
+                    signed.append(False)
+                else:
+                    request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+                    break
+            request.send_response(vals, signed=signed)
+        else:
+            request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+
+    def _process_read_iregs(self, request):
+        """
+        Process access to input register
+
+        Iregs (only getter) [0, 65535], function 04 - read input registers
+
+        :param      request:  The request
+        :type       request:  Request
+        """
+        print('READ_INPUT_REGISTER of ID #{}'.format(request.register_addr))
+
+        if request.register_addr >= 101 and request.register_addr <= 102:
+            vals = []
+            signed = []
+            for i in range(request.register_addr, request.register_addr + request.quantity):
+                if i == 101:
+                    vals.append(101)
+                    signed.append(True)
+                elif i == 102:
+                    vals.append(102)
+                    signed.append(False)
+                else:
+                    request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+                    break
+            request.send_response(vals, signed=signed)
+        else:
+            request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+
+    def _process_write_coils(self, request):
+        """
+        Process setting of coil register
+
+        Coils (only getter) [0, 1], function 05 - write single register
+
+        :param      request:  The request
+        :type       request:  Request
+        """
+        print('WRITE_SINGLE_COIL of ID #{}'.format(request.register_addr))
+        print('Request data: {}'.format(request.data))
+
+        if request.register_addr == 101:
+            val = request.data_as_registers(signed=False)[0]
+            print('Set register to {}'.format(val))
+
+            if request.data[0] == 0x00:
+                print('Set coil to 0x0')
+                request.send_response()
+            elif request.data[0] == 0xFF:
+                print('Set coil to 0xFF')
+                request.send_response()
+            else:
+                request.send_exception(ModbusConst.ILLEGAL_DATA_VALUE)
+        elif request.register_addr == 102:
+            val = request.data_as_registers(signed=False)[0]
+            print('Set register to {}'.format(val))
+
+            if request.data[0] == 0x00:
+                print('Set coil to 0x0')
+                request.send_response()
+            elif request.data[0] == 0xFF:
+                print('Set coil to 0xFF')
+                request.send_response()
+            else:
+                request.send_exception(ModbusConst.ILLEGAL_DATA_VALUE)
+        else:
+            request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
+
+    def _process_write_hregs(self, request):
+        """
+        Process setting of holding register
+
+        Hregs (setter+getter) [0, 65535], function 06 - write holding register
+
+        :param      request:  The request
+        :type       request:  Request
+        """
+        print('WRITE_SINGLE_REGISTER of ID #{}'.format(request.register_addr))
+        print('Request data: {}'.format(request.data))
+
+        if request.register_addr == 101:
+            val = request.data_as_registers(signed=False)
+            print('Set holding register to {}'.format(val))
+            request.send_response()
+        elif request.register_addr == 102:
+            val = request.data_as_registers(signed=False)
+            print('Set holding register to {}'.format(val))
+            request.send_response()
+        else:
+            request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
 
 
 class ModbusRTU(Modbus):
@@ -191,22 +224,31 @@ class ModbusRTU(Modbus):
                    ctrl_pin=ctrl_pin),
             [addr]
         )
-        # self._enable_ap = enable_ap_func
 
+    """
     def _process_req(self, request):
         if request.function == ModbusConst.WRITE_SINGLE_COIL:
-            print('WRITE_SINGLE_COIL on {}'.format(request.register_addr))
+            print('WRITE_SINGLE_COIL of ID #{}'.format(request.register_addr))
+            print('Request data: {}'.format(request.data))
 
             if request.register_addr == 5:
+                val = request.data_as_registers(signed=False)[0]
+                print('Set register to {}'.format(val))
+
                 if request.data[0] == 0xFF:
                     print('Set coil to 0xFF')
-                    # _thread.start_new_thread(self._enable_ap, ())
                     request.send_response()
                 else:
                     request.send_exception(ModbusConst.ILLEGAL_DATA_VALUE)
                 return
+            else:
+                pass
+
+                # let it be processed by Modbus (super) call function
+                # request.send_exception(ModbusConst.ILLEGAL_DATA_ADDRESS)
 
         super()._process_req(request)
+    """
 
 
 class ModbusTCP(Modbus):
@@ -224,5 +266,5 @@ class ModbusTCP(Modbus):
         try:
             return self._itf.get_is_bound()
         except Exception as e:
-            print('Unable to access is_bound flag')
+            print('Unable to access _is_bound flag')
             return False
