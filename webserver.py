36ca154f15f0bd557618c8ebc791b6e0a5cf79f7
Jonas Scharpf <jonas@brainelectronics.de>
2021-10-04 18:24:40 +0200

    add update handling to webserver

--- webserver.py
+++ webserver.py
@@ -70,10 +70,13 @@ class WebServer:
                 return
 
             parts = self._split(first_line, ' ')
+            # ['POST', '/update', 'HTTP/1.1']
+
             req_method = parts[0].upper()
             req_path = parts[1].lower()
 
             req_auth = None
+            content_lenth = None
             while True:
                 line = client_sock.readline()
 
@@ -86,6 +89,10 @@ class WebServer:
                     break
                 if parts[0].lower() == 'authorization':
                     req_auth = parts[1].strip().split()[1]
+                if parts[0].lower() == 'content-length':
+                    content_lenth = int(parts[1].strip())
+
+            print('Given content length: {}'.format(content_lenth))
 
             if req_auth:
                 user_pass = ubinascii.a2b_base64(req_auth).decode().split(':')
@@ -107,7 +114,7 @@ class WebServer:
             print('WebServer - request from: {} of type {} to {}'.
                   format(client_addr, req_method, req_path))
 
-            if req_path == '/config' or req_path == '/config-network':
+            if req_path in ['/config', '/config_network']:
                 filename = "{}.py".format(req_path.strip('/'))
 
                 if req_method == 'POST':
@@ -154,7 +161,7 @@ class WebServer:
                     client_sock.send('Content-Type: text/html\r\n\r\n')
 
                     # reboot if network config changed
-                    if (state == 5) and (req_path == '/config-network'):
+                    if (state == 5) and (req_path == '/config_network'):
                         print('Config uploaded - restarting in 3 sec ...')
                         time.sleep(3)
                         machine.reset()
@@ -164,6 +171,7 @@ class WebServer:
                     client_sock.send('Connection: close\r\n')
 
                     try:
+                        filename = 'config/{}'.format(filename)
                         stat = uos.stat(filename)
                         client_sock.send('Content-Disposition: attachment; filename={}\r\n'.format(filename))
                         client_sock.send('Content-Type: application/force-download\r\n')
@@ -178,6 +186,79 @@ class WebServer:
             elif req_path == '/favicon.ico':
                 client_sock.send('HTTP/1.1 404 Not Found\r\n')
                 client_sock.send('Connection: close\r\n\r\n')
+            elif req_path == '/update' and req_method == 'POST':
+                filename = 'update.tar.gz'
+                tmp_filename = '{}.tmp'.format(filename)
+
+                state = 0
+                received_length = 0
+
+                while True:
+                    # receive data and write it to file onwards
+
+                    # line = client_sock.recv(128)
+                    line = client_sock.readline()
+                    if not line:
+                        break
+                    received_length += len(line)
+
+                    line = line.decode().strip()
+
+                    if state == 0:
+                        keyword = 'filename='
+                        if ((line.startswith('Content-Disposition: ')) and
+                           (keyword in line)):
+                            filename = line.split(keyword)[-1].strip()
+                            filename = filename.replace('"', '')
+                            tmp_filename = '{}.tmp'.format(filename)
+                            state = 1
+                    elif state == 1:
+                        if line == '':
+                            state = 2
+                            f = open(tmp_filename, 'wb')
+                    elif state == 2:
+                        if received_length >= content_lenth:
+                            print('{} byte of file {} received'.
+                                  format(received_length, filename))
+                            f.close()
+                            state = 3
+                            try:
+                                uos.remove(filename)
+                                print('Removed: {}'.format(filename))
+                            except OSError as e:
+                                if e.errno != errno.ENOENT:
+                                    print('Error {} on file {}'.
+                                          format(e, filename))
+                            except Exception as e:
+                                print('Failed to remove "{}" due to {}'.
+                                      format(filename, e))
+                            state = 4
+                            print('Rename {} to {}'.
+                                  format(tmp_filename, filename))
+                            uos.rename(tmp_filename, filename)
+                            print('File operations done')
+                            state = 5
+                            break
+
+                        print('Writing: {}'.format(line))
+                        f.write(line)
+                        f.write('\n')
+
+                # readall is not a valid attribute for socket
+                # client_sock.readall()
+
+                client_sock.send('HTTP/1.1 303 See Other\r\n')
+                client_sock.send('Location: /?state={}\r\n'.format(state))
+                client_sock.send('Content-Type: text/html\r\n\r\n')
+
+                # reboot if network config changed
+                if (state == 5) and 'update' in filename:
+                    print('Dummy reboot here')
+                    """
+                    print('Update uploaded - restarting in 3 sec ...')
+                    time.sleep(3)
+                    machine.reset()
+                    """
             else:
                 client_sock.send('HTTP/1.1 200 OK\r\n')
                 client_sock.send('Connection: close\r\n')
@@ -187,7 +268,8 @@ class WebServer:
                 f = open('index.html', 'rb')
                 client_sock.send(f.read())
         except OSError as e:
-            if e.args[0] != 11:     # 11 = timeout expired
+            # 11 = timeout expired, 116 = ETIMEDOUT
+            if (e.args[0] != 11) and (e.errno != 116):
                 print("WebServer - OSError: {}".format(e))
                 sys.print_exception(e)
         except Exception as e:
