8fb9db164331a04039724f00f06d9f77cf47a227
Jonas Scharpf <jonas@brainelectronics.de>
2022-11-03 10:01:37 +0100

    add float_to_bin, bin_to_float and int_to_bin functions to ubmodbus/functions.py

--- umodbus/functions.py
+++ umodbus/functions.py
@@ -142,3 +142,15 @@ def response(function_code,
 
 def exception_response(function_code, exception_code):
     return struct.pack('>BB', Const.ERROR_BIAS + function_code, exception_code)
+
+
+def float_to_bin(num):
+    return bin(struct.unpack('!I', struct.pack('!f', num))[0])[2:].zfill(32)
+
+
+def bin_to_float(binary):
+    return struct.unpack('!f', struct.pack('!I', int(binary, 2)))[0]
+
+
+def int_to_bin(num):
+    return "{0:b}".format(num)
