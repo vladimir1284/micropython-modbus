479e2edf41a799602fec4c97777ba36c8cee27ca
Jonas Scharpf <jonas@brainelectronics.de>
2023-01-02 23:24:15 +0100

    add test_rtu_example to tests init file as comment, contributes to #47

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,3 +7,6 @@ from .test_functions import *
 
 # TestTcpExample is a non static test and requires a running TCP client
 # from .test_tcp_example import *
+
+# TestRtuExample is a non static test and requires a running RTU client
+# from .test_rtu_example import *
